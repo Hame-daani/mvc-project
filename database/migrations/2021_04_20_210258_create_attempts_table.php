302424484a6e848c067736de94c56dd66d371490
Sadegh Hamedaani <sad3gh.hmp@gmail.com>
2021-04-20 21:15:01 +0430

    create attempts table

---
+++ database/migrations/2021_04_20_210258_create_attempts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAttemptsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attempts', function (Blueprint $table) {
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('quiz_id')->constrained();
+            $table->primary(['user_id', 'quiz_id']);
+            $table->integer('score')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attempts');
+    }
+}
