636d3f43dae1bfea71f873828127a295779ceade
Sadegh Hamedaani <sad3gh.hmp@gmail.com>
2021-04-20 20:17:57 +0430

    update users table based on model

---
+++ database/migrations/2021_04_20_200957_update_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+            $table->integer('scores')->default(100);
+            $table->boolean('is_active')->default(true);
+            $table->boolean('is_admin')->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+            $table->dropColumn('scores');
+            $table->dropColumn('is_active');
+            $table->dropColumn('is_admin');
+        });
+    }
+}
