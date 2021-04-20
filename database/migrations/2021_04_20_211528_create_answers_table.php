2741e9991c3eeb144fcdc5a3dfb040db119bf07c
Sadegh Hamedaani <sad3gh.hmp@gmail.com>
2021-04-20 21:21:59 +0430

    create answers table

---
+++ database/migrations/2021_04_20_211528_create_answers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAnswersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('answers', function (Blueprint $table) {
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('question_id')->constrained();
+            $table->foreignId('option_id')->constrained();
+            $table->primary(['user_id', 'question_id', 'option_id']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('answers');
+    }
+}
