7d803b29d89f86fa3528a65b6510fe74e4988928
Sadegh Hamedaani <sad3gh.hmp@gmail.com>
2021-04-20 20:33:44 +0430

    create_quizzes_table

---
+++ database/migrations/2021_04_20_202817_create_quizzes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuizzesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('quizzes', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->foreignId('book_id')->constrained();
+            $table->foreignId('user_id')->constrained();
+            $table->boolean('is_active')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('quizzes');
+    }
+}
