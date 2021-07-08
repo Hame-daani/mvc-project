3afed43dae5e9383b68e64b846ebe7452f96ad5a
Sadegh Hamedaani <sad3gh.hmp@gmail.com>
2021-07-09 00:20:16 +0430

    checkBanned middleware added

---
+++ app/Http/Middleware/CheckBanned.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class CheckBanned
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (auth()->check() && (auth()->user()->is_active == 0)) {
+            Auth::logout();
+            $request->session()->invalidate();
+            $request->session()->regenerateToken();
+            return redirect()->route('login')->withErrors('Your Account is suspended, please contact Admin.');
+        }
+        return $next($request);
+    }
+}
