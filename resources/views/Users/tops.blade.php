38d5a4b3ecaf20cafc2a49b2a425ee0d642b175b
Sadegh Hamedaani <sad3gh.hmp@gmail.com>
2021-07-11 00:21:35 +0430

    list group for top users

--- resources/views/Users/tops.blade.php
+++ resources/views/Users/tops.blade.php
@@ -1,9 +1,12 @@
 <x-base>
-    <ol>
+    </ol>
+    <ol class="list-group list-group-numbered">
         @foreach ($users as $user)
-            <li>
-                {{ $user->name }} - {{ $user->scores }}
-            </li>
+            <li class="list-group-item d-flex justify-content-between align-items-start">
+                <div class="ms-2 me-auto">
+                    <div class="fw-bold">{{ $user->name }}</div>
+                </div>
+                <span class="badge bg-primary rounded-pill">{{ $user->scores }}</span>
         @endforeach
     </ol>
 </x-base>
