cee3b6413c39608ad11f404b3c1a0f98ea2a1685
Sadegh Hamedaani <sad3gh.hmp@gmail.com>
2021-07-12 21:51:58 +0430

    fix top users style

--- resources/views/Users/tops.blade.php
+++ resources/views/Users/tops.blade.php
@@ -1,12 +1,23 @@
 <x-base>
-    </ol>
-    <ol class="list-group list-group-numbered">
-        @foreach ($users as $user)
-            <li class="list-group-item d-flex justify-content-between align-items-start">
-                <div class="ms-2 me-auto">
-                    <div class="fw-bold">{{ $user->name }}</div>
+    <div class="card">
+        <div class="row g-0 justify-content-center">
+            <div class="col-md-8">
+                <div class="card-body row g-0  justify-content-center">
+                    <h2 class="card-title">Top Users</h2>
+                    <div class="col-md-7">
+                        </ol>
+                        <ol class="list-group list-group-numbered">
+                            @foreach ($users as $user)
+                                <li class="list-group-item d-flex justify-content-between align-items-start">
+                                    <div class="ms-2 me-auto">
+                                        <div class="fw-bold">{{ $user->name }}</div>
+                                    </div>
+                                    <span class="badge bg-primary rounded-pill">{{ $user->scores }}</span>
+                            @endforeach
+                        </ol>
+                    </div>
                 </div>
-                <span class="badge bg-primary rounded-pill">{{ $user->scores }}</span>
-        @endforeach
-    </ol>
+            </div>
+        </div>
+    </div>
 </x-base>
