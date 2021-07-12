0a226db21c855711ff9e4f9e6fa9df85e1979168
Sadegh Hamedaani <sad3gh.hmp@gmail.com>
2021-07-12 18:04:05 +0430

    add style to create book page

--- resources/views/Books/create.blade.php
+++ resources/views/Books/create.blade.php
@@ -1,23 +1,33 @@
 <x-base>
-    <h1>creating new book:</h1>
-    <form action="{{ route('books.store') }}" method="POST">
-        @csrf
-        <label for="title">title:</label>
-        <input type="text" name="title" id="title">
-        <br>
-        <br>
-        <label for="info">info:</label>
-        <textarea name="info" id="info" cols="100" rows="5"></textarea>
-        <br>
-        <br>
-        <label for="title">link:</label>
-        <input type="url" name="link" id="link">
-        <br>
-        <br>
-        <label for="title">img url:</label>
-        <input type="url" name="img" id="img">
-        <br>
-        <br>
-        <input type="submit" value="create">
-    </form>
+    <div class="container">
+        <div class="card">
+            <div class="row g-0 justify-content-center">
+                <div class="col-md-5">
+                    <div class="card-body">
+                        <h1 class="card-title">Create New Book</h1>
+                        <form action="{{ route('books.store') }}" method="POST">
+                            @csrf
+                            <div class="mb-3">
+                                <label for="title" class="form-label">Title</label>
+                                <input type="text" class="form-control" name="title" id="title">
+                            </div>
+                            <div class="mb-3">
+                                <label for="info" class="form-label">Info</label>
+                                <textarea class="form-control" name="info" id="id" cols="100" rows="5"></textarea>
+                            </div>
+                            <div class="mb-3">
+                                <label for="link" class="form-label">Link</label>
+                                <input type="url" class="form-control" name="link" id="link">
+                            </div>
+                            <div class="mb-3">
+                                <label for="img" class="form-label">Image Link</label>
+                                <input type="url" class="form-control" name="img" id="img">
+                            </div>
+                            <button type="submit" class="btn btn-primary">Create</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </x-base>
