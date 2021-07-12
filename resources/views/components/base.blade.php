c5b6f238cc4b9b9dc7f9e2c35be65bae3bd0dfeb
Sadegh Hamedaani <sad3gh.hmp@gmail.com>
2021-07-13 00:49:58 +0430

    add vazir font

--- resources/views/components/base.blade.php
+++ resources/views/components/base.blade.php
@@ -15,7 +15,14 @@
     {{-- <link rel="stylesheet" href="{{ asset('css/app.css') }}"> --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/gh/rastikerdar/vazir-font@v29.1.0/dist/font-face.css" rel="stylesheet"
+        type="text/css" />
+    <style>
+        * {
+            font-family: 'Vazir', sans-serif;
+        }
 
+    </style>
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
 </head>
