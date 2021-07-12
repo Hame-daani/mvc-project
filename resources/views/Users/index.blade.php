0cdeba4f479cf6f9a871bc4cc1a64541191c2c90
Sadegh Hamedaani <sad3gh.hmp@gmail.com>
2021-07-12 15:18:26 +0430

    add style to users index

--- resources/views/Users/index.blade.php
+++ resources/views/Users/index.blade.php
@@ -1,25 +1,36 @@
 <x-base>
-    @foreach ($users as $user)
-        <ul>
-            <li><a href="{{ route('users.show', $user->id) }}">{{ $user->name }}</a></td>
-                @if ($user->is_active)
-                    &#9989;
-                @else
-                    &#10060;
-                @endif
-                @if ($user->is_admin)
-                    &#128293;
-                @else
-                    &#9872;
-                @endif
-                <form action="{{ route('users.toggle', ['user' => $user->id]) }}" method="post">
-                    @csrf
-                    <input type="submit" name="toggle" value="{{ $user->is_active ? 'deactive' : 'activate' }}">
-                </form>
-                <form action="{{ route('users.admin', ['user' => $user->id]) }}" method="post">
-                    @csrf
-                    <input type="submit" name="toggle" value="{{ $user->is_admin ? 'unadmin' : 'admin' }}">
-                </form>
+    <div class="row g-0 justify-content-center">
+        <ul class="list-group col-md-8">
+            @foreach ($users as $user)
+                <li
+                    class="list-group-item justify-content-between d-flex @unless($user->is_active) list-group-item-danger @endunless">
+                    <div>
+                        <a href="{{ route('users.show', $user->id) }}">
+                            {{ $user->name }}
+                        </a>
+                        &nbsp;
+                        @if ($user->is_admin)
+                            <span class="badge rounded-pill bg-warning text-dark">Admin</span>
+                        @endif
+                    </div>
+                    <div class="row">
+                        <div class="col-5 col-md-6">
+                            <form action="{{ route('users.toggle', ['user' => $user->id]) }}" method="post">
+                                @csrf
+                                <input type="submit" class="btn @if ($user->is_active) btn-danger @else btn-success @endif" name="toggle"
+                                value="{{ $user->is_active ? 'deactive' : 'activate' }}">
+                            </form>
+                        </div>
+                        <div class="col-5 col-md-6">
+                            <form action="{{ route('users.admin', ['user' => $user->id]) }}" method="post">
+                                @csrf
+                                <input type="submit" class="btn @if ($user->is_admin) btn-secondary @else btn-warning @endif" name="toggle"
+                                value="{{ $user->is_admin ? 'unadmin' : 'admin' }}">
+                            </form>
+                        </div>
+                    </div>
+                </li>
+            @endforeach
         </ul>
-    @endforeach
+    </div>
 </x-base>
