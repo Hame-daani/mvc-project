f20789b3cb0e65fb57ae55b3a5bcefd8e841ee24
Sadegh Hamedaani <sad3gh.hmp@gmail.com>
2021-07-12 18:17:13 +0430

    add style to create quiz page

--- resources/views/Quizzes/create.blade.php
+++ resources/views/Quizzes/create.blade.php
@@ -1,9 +1,22 @@
 <x-base>
-    <h1>{{ $book->title }}</h1>
-    <form action="{{ route('books.quizzes.store', ['book' => $book->id]) }}" method="post">
-        @csrf
-        <label for="title">title:</label>
-        <input type="text" name="title" id="title">
-        <input type="submit" value="submit">
-    </form>
+    <div class="container">
+        <div class="card">
+            <div class="row g-0 justify-content-center">
+                <div class="col-md-5">
+                    <div class="card-body">
+                        <h1 class="card-title">Create New Quiz</h1>
+                        <p>This quiz will created for {{ $book->title }} book.</p>
+                        <form action="{{ route('books.quizzes.store', ['book' => $book->id]) }}" method="post">
+                            @csrf
+                            <div class="mb-3">
+                                <label for="title" class="form-label">Title</label>
+                                <input type="text" class="form-control" name="title" id="title" required>
+                            </div>
+                            <button type="submit" class="btn btn-primary">Create</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </x-base>
