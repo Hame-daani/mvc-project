acf39f5add57f071bf81be5fd1c9919bdfec86a2
Sadegh Hamedaani <sad3gh.hmp@gmail.com>
2021-07-13 21:44:02 +0430

    rewrite footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -2,20 +2,19 @@
     <div class="container p-4 pb-0">
         <section class="row">
             <p class="d-flex justify-content-center align-items-center">
-                <span class="me-3">تمامی کتاب‌ها در این سایت از طریق پروژه Gutenberg تهیه شده است.</span>
+                <span class="me-3">تمامی کتاب‌ها در این سایت از طریق پروژه <a class="text-dark"
+                        href="https://www.gutenberg.org/">Gutenberg</a> تهیه شده اند.</span>
             </p>
             <p class="d-flex justify-content-center align-items-center">
-                <span class="me-3">تمامی کوییزها نیز از طریق سایت sparknotes تهیه شده‌اند.</span>
+                <span class="me-3">تمامی کوییزها نیز از طریق سایت <a class="text-dark"
+                        href="https://www.sparknotes.com/">Sparknotes</a> تهیه شده‌اند.</span>
             </p>
         </section>
     </div>
     <!-- Copyright -->
     <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);" dir="ltr">
         © 2021 Copyright:
-        <br>
-        <a class="text-dark" href="https://www.gutenberg.org/">Gutenberg Project</a>
-        <br>
-        <a class="text-dark" href="https://www.sparknotes.com/">Sparknotes</a>
+        <a class="text-dark" href="https://github.com/Hame-daani/mvc-project">Sadegh Hamedani</a>
     </div>
     <!-- Copyright -->
 </footer>
