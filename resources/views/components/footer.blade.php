8563e64811a6e80e825ac6566e4b58a8fdddefab
Sadegh Hamedaani <sad3gh.hmp@gmail.com>
2021-07-13 20:17:43 +0430

    footer fixed

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,8 +1,10 @@
 <footer class="bg-light text-center text-lg-start">
     <div class="container p-4 pb-0">
-        <section class="">
+        <section class="row">
             <p class="d-flex justify-content-center align-items-center">
                 <span class="me-3">تمامی کتاب‌ها در این سایت از طریق پروژه Gutenberg تهیه شده است.</span>
+            </p>
+            <p class="d-flex justify-content-center align-items-center">
                 <span class="me-3">تمامی کوییزها نیز از طریق سایت sparknotes تهیه شده‌اند.</span>
             </p>
         </section>
@@ -10,7 +12,9 @@
     <!-- Copyright -->
     <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
         © 2021 Copyright:
+        <br>
         <a class="text-dark" href="https://www.gutenberg.org/">Gutenberg Project</a>
+        <br>
         <a class="text-dark" href="https://www.sparknotes.com/">Sparknotes</a>
     </div>
     <!-- Copyright -->
